e252cf6a9273a5ef511129a339a1481da4fc63d3
Mikolaj Klosek <mikolaj.klosek@gmail.com>
2023-11-27 21:08:27 +0100

    changed index.js syntax error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import {GlobalStyle} from "./GlobalStyle";
+import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from './theme';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
