0ffed89bd128f56e0c2e1acc3ce655ea7a0800d5
Mikolaj Klosek <mikolaj.klosek@gmail.com>
2023-11-27 21:03:38 +0100

    corrected error

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import { createGlobalStyle } from "styled-components";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import {GlobalStyle} from "./GlobalStyle";
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { theme } from './theme';
 
-export const GlobalStyle = createGlobalStyle`
-    html {
-        box-sizing: border-box;
-    }
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-    *, ::after, ::before {
-        box-sizing: inherit;
-    }
-
-    body {
-        font-family: 'Montserrat', sans-serif;
-        background: ${({ theme }) => theme.color.gallery};
-    }
-
-    button {
-        cursor: pointer;
-    }
-`;
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
